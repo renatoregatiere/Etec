efc57c79b5bc446c3467c89e63d0abab47d5dfc9
Renato Regatiere <renatoregatiere@gmail.com>
2023-10-03 22:21:11 -0300

    +

--- IntefaceWeb II/Form_Renato/js/validaForm.js
+++ IntefaceWeb II/Form_Renato/js/validaForm.js
@@ -9,7 +9,30 @@ function validaTexto() {
         e = document.getElementById('endereco').value;
 
         resp = document.getElementById('caixaResposta');
-        resp.innerHTML = 'Nome ' + n + ' Telefone ' + t + ' Endereço ' + e;
+        resp.innerHTML = 'Nome ' + n + 'Telefone ' + t + '\n Endereço ' + e;
     }
 
+}
+
+function validaCheck() {
+    var check, qtdCheck, i, qtdSelecionado = 0,
+        ling = "";
+    check = document.getElementsByName('linguagem');
+    qtdCheck = check.length;
+    for (i = 0; i < qtdCheck; i++) {
+        if (check[i].checked)
+            qtdSelecionado++;
+    }
+    if (qtdSelecionado == 0) {
+        alert('Escolha pelo menos uma linguagem');
+        return false;
+    } else {
+        check = document.getElementsByName("linguagem");
+        qtdCheck = check.length;
+        for (i = 0; i < qtdCheck; i++)
+            if (check[i].checked)
+                ling = ling + ' ' + check[i].value;
+    }
+    resp = document.getElementById('caixaResposta');
+    resp.innerHTML = 'Linguagens Selecionadas:' < br > +ling;
 }
