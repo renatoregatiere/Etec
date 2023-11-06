074c7b55569f3f0ebecdae02b5f070d0cdc9f3f7
Renato Regatiere <renatoregatiere@gmail.com>
2023-11-06 20:53:58 -0300

    +

--- IntefaceWeb II/dom_Renato/js/scriptDom3.js
+++ IntefaceWeb II/dom_Renato/js/scriptDom3.js
@@ -1,8 +1,8 @@
 a1 = document.getElementById('area1');
 a1.addEventListener('mousedown', clicouMouse);
-a1.addEventListener('mouseup', soutouMouse);
+a1.addEventListener('mouseup', soltouMouse);
 
 function clicouMouse() {
-    a1.style.backgroundColor='blue';
-    a1.innerHTML='Solte o clique';
+    a1.style.backgroundColor = 'blue';
+    a1.innerHTML = 'Solte o clique';
 }
