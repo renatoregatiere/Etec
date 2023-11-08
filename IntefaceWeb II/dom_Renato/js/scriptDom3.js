8cf4f3bdc3b2f3538732940659af39bee3009484
Renato Regatiere <renatoregatiere@gmail.com>
2023-11-07 21:30:40 -0300

    Update scriptDom3.js

--- IntefaceWeb II/dom_Renato/js/scriptDom3.js
+++ IntefaceWeb II/dom_Renato/js/scriptDom3.js
@@ -2,7 +2,24 @@ a1 = document.getElementById('area1');
 a1.addEventListener('mousedown', clicouMouse);
 a1.addEventListener('mouseup', soltouMouse);
 
+a2 = document.getElementById('area2');
+a2.addEventListener('mouseOver', passouMouse);
+a2.addEventListener('mouseout', tirouMouse);
+
 function clicouMouse() {
     a1.style.backgroundColor = 'blue';
     a1.innerHTML = 'Solte o clique';
+}
+
+function soltouMouse() {
+    a1.style.backgroundColor = 'orange';
+    a1.innerHTML = 'Valeu!';
+}
+
+function passouMouse() {
+    a2.innerHTML = 'obrigado!';
+}
+
+function tirouMouse() {
+    a2.innerHTML = 'Passe o mouse!';
 }
