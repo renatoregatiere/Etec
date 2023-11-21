363431c53632af4c2920aad6d22e9101f0bc156a
Renato Regatiere <renatoregatiere@gmail.com>
2023-11-21 20:35:19 -0300

    -

--- Sistemas Web I/Farmacia/build/web/WEB-INF/classes/br/com/livraria/util/livraria.sql
+++
@@ -1,9 +0,0 @@
-create table livro(
-	idlivro integer auto_increment primary key not null,
-	nomelivro varchar(40),
-	generolivro varchar(40),
-	autorlivro varchar(30),
-	resumolivro varchar(180),
-	precolivro numeric(7,2),
-	isbnlivro varchar(13)
-);
